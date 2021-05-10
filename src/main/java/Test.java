651d001d0637a0d05e8c892ef571fd3fcc9f78e8
CPZ7758521 <83965690+CPZ7758521@users.noreply.github.com>
2021-05-11 01:37:10 +0800

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("master");
         System.out.println("hot-fix");
         System.out.println("xinzeng");
+        System.out.println("xinzeng");
     }
 }
